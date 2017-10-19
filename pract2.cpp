214ab7ae60ca54f27bf38239d0b1d2fb764a14da
Miriam12 <32524062+Miriam12@users.noreply.github.com>
2017-10-19 13:19:10 -0500

    Update pract2.cpp

--- pract2.cpp
+++ pract2.cpp
@@ -49,6 +49,81 @@ int main(int argc, char** argv) {
 		scanf("%d",&r);	
 			
 	}while (r==1);
+	//COMPUTADORA
+     printf (" !!!TURNO DE LA COMPUTADORA !!!\n");
+        do {
+		if(intento_c==0){
+			cartas_c=2;
+			sum_cartas+=2;
+		
+			}
+			else {
+			cartas_c=1;	
+			sum_cartas+=1;
+			}			
+	
+				
+	for(int i=0; i<cartas_c;i++) {
+		
+		valor=1+rand()%(52-1);
+			
+			switch(cartas[valor]){
+			case 'A': 
+			if(intento_c==0 ){
+				puntos_c+=11;
+			}				
+			else{
+				puntos_c+=1;				
+			}
+			printf("LA CARTA DE LA COMPUTADORA ES: %c \n",cartas[valor]);
+			break;
+			case 'J':case 'Q':case 'K':
+				puntos_c+=10;	
+				printf("LA CARTA DE LA COMPUTADORA ES: %c \n",cartas[valor]);
+			break;
+			
+			default: 
+			puntos_c+=cartas[valor];	
+			printf("LA CARTA DE LA COMPUTADORA ES: %d \n",cartas[valor]);		
+			break;
+				}
+			}
+			printf("LA PUNTUACION DE LA COMPUTADORA ES: %d \n",puntos_c);	
+			
+			if (intento_c==0 && puntos_c <18){
+				t=1;			
+			}				
+			if (sum_cartas>=3 && puntos_c>=18){
+				t=0;					
+			}	
+			if (intento_c==0 && puntos_c >=18){
+				t=0;			
+			}
+			if (sum_cartas>=3 && puntos_c<18){
+				t=1;					
+			}							
+			
+				intento_c=1;		
+	}while (t==1);
+	
+	if(b==1){
+		printf("PERDISTE");			
+}
+    else if (puntos_c>21)  {
+			printf("GANASTE\n");		
+			printf("PERDIO LA COMPUTADORA");			
+			}
+	else if (puntos_c> puntos)	{
+		printf("PERDISTE\n");	
+		printf("GANO LA COMPUTADORA");		
+}	
+    else	{
+		printf("GANASTE\n");	
+		printf("PERDIO LA COMPUTADORA");		
+}	
+			
+	return 0;
+}
 	
 
 	return 0;
